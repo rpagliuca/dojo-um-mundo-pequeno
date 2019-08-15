c99618613093536356ae18761f6580ef8170ffc9
Rafael Santos <rafael.santos.193@gmail.com>
2019-08-15 14:29:57 -0300

    feat: 100% pure functions

--- sort.go
+++ sort.go
@@ -1,7 +1,9 @@
 package main
 
 /* Sort list of distances */
-func sort_distances(distances []Distance) []Distance {
+func sort_distances(original_distances []Distance) []Distance {
+    distances := make ([]Distance, len(original_distances))
+    copy(distances, original_distances)
     for i := 0; i < len(distances); i++ {
         for j := i + 1; j < len(distances); j++ {
             if distances[i].distance > distances[j].distance {
